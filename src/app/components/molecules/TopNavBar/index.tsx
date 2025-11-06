8126376d2cbc95e42a2ca6cae10e77bb22b95f2e
karim <karimyakoub1515@gmail.com>
2025-11-06 11:51:27 -0500

    initial cleanup

---
+++ src/app/components/molecules/TopNavBar/index.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function TopNavBar(){
+    return (
+        <div className="bg-red-500 h-12">
+            <Link href=''>Vocab Tracker</Link>
+        </div>
+    )
+
+}
